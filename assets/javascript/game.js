15f6b375666a59fa6f2c1507d5f2afaca0345df2
kmalillos <malillosk@gmail.com>
2018-10-13 14:58:47 -0600

    changed html and js to create crystals by js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,9 +10,28 @@ var userScore = 0;
 var wins = 0;
 var losses = 0; 
 
+var crystalArray = [
+    "assets/images/crystal-1.jpg",
+    "assets/images/crystal-2.jpg",
+    "assets/images/crystal-3.jpg",
+    "assets/images/crystal-4.jpg"
+];
 
 
 // --- Functions ---
+
+// CREATE crystal images
+function createCrystals() {
+    for (var i=0; i<crystalArray.length; i++) {
+        var rand = Math.floor(Math.random()*12 + 1);
+        var img = $("<img>")
+            img.addClass('crystal-img');
+            img.attr('id', 'crystal-'+(i+1));
+            img.attr('src', crystalArray[i]);
+            img.attr('val', rand)
+        $("#crystals").append(img);
+    }
+}
 
 // for TARGET SCORE to get a RANDOM NUM between 19-120 (120-19 = 101)
 function targetScoreSelector () {
@@ -21,12 +40,12 @@ function targetScoreSelector () {
 }
 
 // for CRYSTAL to get a RANDOM VALUE between 1-12 
-var crystalValueSelector = function () {
-    $("#crystal-1").attr('val',  Math.floor(Math.random()*12 + 1));
-    $("#crystal-2").attr('val',  Math.floor(Math.random()*12 + 1));
-    $("#crystal-3").attr('val',  Math.floor(Math.random()*12 + 1));
-    $("#crystal-4").attr('val',  Math.floor(Math.random()*12 + 1));
-}
+// var crystalValueSelector = function () {
+//     $("#crystal-1").attr('val',  Math.floor(Math.random()*12 + 1));
+//     $("#crystal-2").attr('val',  Math.floor(Math.random()*12 + 1));
+//     $("#crystal-3").attr('val',  Math.floor(Math.random()*12 + 1));
+//     $("#crystal-4").attr('val',  Math.floor(Math.random()*12 + 1));
+// }
 
 
 function resetGame () {
@@ -40,14 +59,11 @@ function resetGame () {
 
 // --- Main Process ---
 
+// CREATE crystals
+createCrystals();
+
 // to SELECT a RANDOM targetScore
 targetScoreSelector();
-
-//give crystal random value
-
-//increase user score per random crystal value selected
-
-crystalValueSelector();
 
 $(".crystal-img").on("click", function () {
     //alert("This is a crystal");
@@ -73,14 +89,6 @@ $(".crystal-img").on("click", function () {
         $("#losses-text").text(losses);
         resetGame();
     }
-
-
-// HTML Elements
-
-// $("#targetScore-text").text(targetScore);
-// $("#userScore-text").text(userScore);
-// 
-// 
 
 });
 
